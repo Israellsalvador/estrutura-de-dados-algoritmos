a7fe6956ae24bd8968ff3340fb7cb80fc4b36dc2
Israel Salvador <150477178+Israellsalvador@users.noreply.github.com>
2025-08-22 23:31:28 +0000

    aula3

---
+++ aula03/Principal.java
@@ -0,0 +1,16 @@
+public class Principal{
+    public static void main(String [] args){
+
+
+        Lista<String> lista = new Lista<String>("Alunos");
+
+        
+
+        lista.addInicio("Rafael");
+        lista.addInicio("Gabriel");
+        lista.imprimeLista();
+        lista.addFinal("Daniel");
+        lista.imprimeLista();
+    }
+}
+
